2df90fed7237be2a00d5483a6cadee795567042f
Bhakti Sawarkar <bhakti123.java@gmail.com>
2018-08-18 02:40:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function breakOut(array, changeValue, stopValue){
 function keepGoing(array, changeValue, skipValue){
   for(var i = 0; i < array.length; i++)
   {
-    if (array[i] !== skipValue){
+    if (array[i] !== stopValue){
       continue;
     } else {
       array[i] = changeValue;
